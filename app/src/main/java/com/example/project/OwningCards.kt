be2eacbb1b11a091e1caab5d39f6871db6dc134e
ruasu6 <jieni0910@gmail.com>
2024-07-21 13:52:44 +0800

    new upload

---
+++ app/src/main/java/com/example/project/OwningCards.kt
@@ -0,0 +1,52 @@
+package com.example.project
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun OwningCards() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(android.graphics.Color.parseColor("#f2f1f6")))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "我的卡牌", fontSize = 40.sp, fontWeight = FontWeight.SemiBold)
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = Modifier
+                .padding(top = 16.dp)
+
+        ) {
+            items(12) { index ->
+                Box(
+                    modifier = Modifier
+                        .aspectRatio(3 / 4f)
+                        .padding(8.dp)
+                        .background(Color.Gray)
+                        .clickable {
+                        }
+                )
+            }
+        }
+    }
+}
